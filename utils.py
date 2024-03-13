3b0b397b15dcd4838db261619a71c65d5764f716
pchxp <31230133+pchxp@users.noreply.github.com>
2024-03-13 13:01:49 +0900

    Update utils.py
    query_refiner bug fixing

--- utils.py
+++ utils.py
@@ -19,8 +19,8 @@ def find_match(input):
 
 def query_refiner(conversation, query):
 
-    response = openai.Completion.create(
-    model="text-davinci-003",
+    response = openai.completions.create(
+    model="gpt-3.5-turbo-instruct",
     prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
     temperature=0.7,
     max_tokens=256,
@@ -28,7 +28,7 @@ def query_refiner(conversation, query):
     frequency_penalty=0,
     presence_penalty=0
     )
-    return response['choices'][0]['text']
+    return response.choices[0].text
 
 def get_conversation_string():
     conversation_string = ""
